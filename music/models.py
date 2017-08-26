ee59a41df87e48a50371024e71fef40703edf4b9
paulan <paulan94@gmail.com>
2017-08-26 00:07:06 -0700

    videos 6-8 about migrations and sqllite3

--- music/models.py
+++ music/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+#python manage.py makemigrations music
+
+
+#Album red has prim key= 1
+class Album(models.Model): #creates 4 columns behind the scenes. First class has ID of 1
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000) #url
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE) #ondelete, when album is delete, any songs go away too.
+    file_type = models.CharField(max_length=10)
+    song_title=models.CharField(max_length=250)
